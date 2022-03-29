e036900896a5e974e042b081553a72f73fc0d6b0
maiyzaza <maiy27982@gmail.com>
2022-03-30 02:02:03 +0700

    asdasd

--- db/models/accounts.js
+++ db/models/accounts.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-var TransectionSchema = new Schema({
+var AccountSchema = new Schema({
   code: String,
   firstName: String,
   lastName: String,
   balance: Number
 });
 
-module.exports = mongoose.model('Transections', TransectionSchema, "transections", {strict: true});
+module.exports = mongoose.model('Accounts', AccountSchema, "accounts", {strict: true});
--- db/models/transections.js
+++ db/models/transections.js
@@ -9,8 +9,4 @@ var TransectionSchema = new Schema({
   createAt: Date
 });
 
-if (mongoose.models.Transections) {
-  delete mongoose.models.Transections
-}
-
 module.exports = mongoose.model('Transections', TransectionSchema, "transections", {strict: true});
