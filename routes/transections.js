721ba0760c70c355928cbd7a7d41adc1881612fd
maiyzaza <maiy27982@gmail.com>
2022-03-31 12:55:24 +0700

    assadasd

--- routes/transections.js
+++ routes/transections.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 const mongoose = require("mongoose");
 var Transections = require("../db/models/transections");
+var Account = require("../db/models/accounts")
 
 /* GET products listing. */
 router.get("/", (req, res, next) => {
@@ -79,6 +80,7 @@ router.get("/year", (req, res, next) => {
 
 // Create new product
 router.post("/", (req, res, next) => {
+  var account1 = await Account.findOne({});
   const data = req.body;
   const transection1 = new Transections({
     code: data.code,
@@ -86,6 +88,7 @@ router.post("/", (req, res, next) => {
     amount: data.amount,
     createAt: data.createAt
   });
+  account1.balance -= data.amount
   transection1.save((err, newInstance) => {
     if (err) {
       console.error("Hey look, Error!", err);
@@ -94,6 +97,7 @@ router.post("/", (req, res, next) => {
       res.json(newInstance);
     }
   });
+  await account1.save();
 });
 
 router.delete("/:id", (req, res, next) => {
@@ -109,15 +113,18 @@ router.delete("/:id", (req, res, next) => {
 });
 
 router.put("/:id", async (req, res, next) => {
+  var account1 = await Account.findOne({});
   const data = req.body;
   const id = req.params.id;
   var transection1 = await Transections.findOne({ _id: id });
+  account1.balance += transection1.amount
   transection1.code = data.code;
   transection1.categoryName = data.categoryName;
   transection1.amount = data.amount;
   transection1.createAt = data.createAt;
-
+  account1.balance -= data.amount
   await transection1.save();
+  await account1.save();
   res.status(200).json(transection1);
 });
 
